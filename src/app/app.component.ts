8c9a3d0f459f3901904afb8031c6d621b51e707c
Pierre Vicq <pierrevicq@gmail.com>
2018-06-06 11:14:54 +0200

    Main page done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons';
 
 @Component({
   selector: 'pokemon-app',
-  template: `<h1>Hey {{name}}</h1>`,
+  templateUrl: './app/app.component.html'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent implements OnInit { 
+  private pokemons: Pokemon[];
+
+  private title: string = "Liste de plusieurs Pokémons";
+
+  ngOnInit() {
+    this.pokemons = POKEMONS;
+  }
+
+  selectPokemon (pokemon: Pokemon) {
+    alert("vous avez sélectionné le Pokemon " + pokemon.name);
+  }
+
+}
